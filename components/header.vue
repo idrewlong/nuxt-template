eddd077e9c121aa7e3c7acb4d74658d1de314ca6
Richard Andrew Long <id-dev@Richards-MacBook-Air.local>
2024-08-30 23:23:34 -0500

    added seo, purgecss, gtag

--- components/header.vue
+++ components/header.vue
@@ -1,3 +1,11 @@
 <template>
-  <div class="flex">Header</div>
+  <div class="flex flex-row gap-4">
+    <Logo />
+    <!-- Internal Link -->
+    <NuxtLink to="/" rel="next" class="text-black no-underline">Home</NuxtLink>
+    <!-- External Link -->
+    <NuxtLink to="/" rel="external" class="text-black no-underline"
+      >Other</NuxtLink
+    >
+  </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,15 @@ export default defineNuxtConfig({
   alias: {
     '@': resolve(__dirname, "/")
   },
-
+  site: {
+    url: 'https://example.com',
+    name: 'Awesome Site',
+    description: 'Welcome to my awesome site!',
+    defaultLocale: 'en', // not needed if you have @nuxtjs/i18n installed
+  },
+  gtag: {
+    id: 'G-XXXXXXXXXX'
+  },
   devtools: { enabled: true },
   css: ['~/assets/scss/main.scss'],
   image: {
@@ -17,6 +25,9 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  purgecss: {
+    enabled: true, // Always enable purgecss
+  },
 
-  modules: ["@nuxt/image", "@nuxt/fonts"]
+  modules: ["@nuxt/image", "@nuxt/fonts", "@nuxtjs/seo", 'nuxt-gtag', 'nuxt-purgecss']
 })
