c66918fb0c535c016f69efa3f0d4600bfd555729
Richard Andrew Long <id-dev@Richards-MacBook-Air.local>
2024-09-10 19:55:51 -0500

    small change

--- components/header.vue
+++ components/header.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="flex flex-row gap-4">
-    <Logo />
+    <img
+      src="https://placeholder.pics/svg/640x480"
+      alt="Placeholder"
+      loading="lazy"
+      width="640"
+      height="480"
+      title="Placeholder"
+    />
     <!-- Internal Link -->
     <NuxtLink to="/" rel="next" class="text-black no-underline">Home</NuxtLink>
     <!-- External Link -->
